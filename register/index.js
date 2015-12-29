69abbff09c04404badb84e0ad81fd26322302d0f
FG Ribreau <github@fgribreau.com>
2015-12-29 11:12:05 +0100

    docs(register)

--- register/index.js
+++ register/index.js
@@ -1,3 +1,5 @@
+// sadly:
 // const {match, when} = require('..');
+// does not work with node v5.3.0 and I don't want a compile step.
 global.match = require('..').match;
 global.when = require('..').when;
